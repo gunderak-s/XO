2f9b1821988179a1ed035af0260eb50f6f4b3693
oreletska <olena482fly@gmail.com>
2021-10-23 20:19:01 +0300

    виправила метод

--- src/com/company/GameXO.java
+++ src/com/company/GameXO.java
@@ -30,16 +30,10 @@ public class GameXO {
         return true;
     }
 
-    boolean isEndGame(char playerSymbo1) {
-      boolean result=false;
-      if (checkWin(playerSymbo1, map)) {
-          System.out.println("Виграли" +playerSymbo1 + '!');
-          result=true;
-      }
-       if (isMapFull()&& !checkWin(playerSymbo1, map)) {
-           System.out.println("Нічия");
-           result=true;
-       }
-      return result;
+    boolean isEndGame() {
+        if ((field[0][0]=='X') &(field[1][0]=='X') &(field[2][0]=='X'))
+        return true;
+
+
     }
 }

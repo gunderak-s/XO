3d9dd4b7c27da6db0a4676ecb5d84c05cf3e5b81
VladimirKing <cheremuha1@gmail.com>
2021-10-23 19:22:05 +0300

    рандомний хід

--- src/com/company/GameXO.java
+++ src/com/company/GameXO.java
@@ -7,11 +7,22 @@ public class GameXO {
 
 
     public void randomComp(){
-        Random comp=new Random();
+        Random random=new Random();
+        boolean good=true;
+        while (good) {
+            int x = random.nextInt(3);
+            int y = random.nextInt(3);
+            if (field[y][x] == ' ') {
+                field[y][x] = 'O';
+                good=false;
+            } else {good=true;
+            }
+        }
     }
 
 
     void printField () {
+
 
     }
 
